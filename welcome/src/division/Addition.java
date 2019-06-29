396c628330b3d8049c7f972e878fe631e3e805ad
Redington <Redington@LAPTOP-76H4OH5D>
2019-06-29 10:01:03 +0530

    FirstExcersise-Addition

---
+++ welcome/src/division/Addition.java
@@ -0,0 +1,12 @@
+package division;
+
+public class Addition {
+int amount1,amount2;
+
+void add()
+{
+	amount1=100;
+	amount2=200;
+	System.out.println(amount1+amount2);
+	}
+}
---
+++ welcome/src/division/AdditionTest.java
@@ -0,0 +1,11 @@
+package division;
+
+public class AdditionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Addition a1=new Addition();
+a1.add();
+	}
+
+}
